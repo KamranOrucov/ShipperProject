8663c0746b419164a37799d8fa3eb3ea246fa70f
Kamran <kamran.o@code.edu.az>
2016-05-29 14:53:01 +0400

    first commit

---
+++ js/js.js
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+   <head>
+   	 <meta charset="utf-8">
+  <link rel="stylesheet" type="text/js" href="../shipper.html">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+    <title></title>
+
+   </head>
+   <body>
+
+  <script>
+  	$ (document).ready (function () {
+  		
+  	})
+
+
+
+
+  </script>
+  
+
+
+
+
+
+
+
+
+
+   </body>
